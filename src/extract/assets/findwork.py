66df836f9261eba6d237899bf2b9d2de2aa5649f
JesSchattschneider <jessica.leiria@gmail.com>
2024-05-28 18:11:33 +1200

    add: transform functions and dealing with different location names across dataframes, still needs work

--- src/extract/assets/findwork.py
+++ src/extract/assets/findwork.py
@@ -1,9 +1,13 @@
-import sys
+from jinja2 import Environment
 import pandas as pd
 from src.extract.connectors.findwork_api import FindWorkApiClient
 from pathlib import Path
 from sqlalchemy import Table, MetaData
 from src.extract.connectors.postgresql import PostgreSqlClient
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut
+from graphlib import TopologicalSorter
+import time
 
 
 def extract_jobs(
@@ -12,12 +16,13 @@ def extract_jobs(
     """
     Extract job listings based on the search query and location.
     """
+    # todo:
+    # check if findwork_data already exists in the db, if so, get the last page number
+    # if not, set page to list [1:60]
+
     jobs_data = findwork_api_client.get_jobs(
         search_query=search_query, location=location, page=page)
-
-    # df_jobs = pd.json_normalize(jobs_data)
-    # print(df_jobs.head())
-
+    
     # Print the keys of the 'jobs' dictionary
     print("Keys in the 'jobs' dictionary:", jobs_data.keys())
 
@@ -26,12 +31,11 @@ def extract_jobs(
 
     # place the results in a dataframe
     df_jobs_results = pd.json_normalize(jobs_data['results'])
-    print(df_jobs_results.head())
 
     # Print the number of job listings in the 'results' list
     print("Number of job listings in the 'results' list:",
           len(jobs_data['results']))
-
+    
     return df_jobs_results
 
 
@@ -40,18 +44,49 @@ def extract_population(population_reference_path: Path) -> pd.DataFrame:
     df_population = pd.read_csv(population_reference_path)
     return df_population
 
+# Initialize geocoder with a longer timeout
+geolocator = Nominatim(user_agent="job_location_parser", timeout=1)
+
+def _parse_location(location):
+    # Handle NA or None values
+    if pd.isna(location) or location.upper() == "NA":
+        return ("Unknown", "Unknown")
+
+    # Handle remote jobs separately
+    if "REMOTE" in location.upper():
+        return ("Remote", "Remote")
+    
+    # Handle remote jobs with no location specified separately
+    if location.upper() == "NONE":
+        return ("Unknown", "Unknown")
+
+    attempt = 0
+    while attempt < 5:
+        try:
+            # Geocode the location
+            location_geo = geolocator.geocode(location, language='en')
+            if location_geo:
+                # Split the address into components
+                address_parts = location_geo.address.split(',')
+                country = address_parts[-1].strip()
+                # Assume the city is the second to last part
+                city = address_parts[-3].strip() if len(address_parts) > 2 else address_parts[0].strip()
+                return (city, country)
+            else:
+                return ("Unknown", "Unknown")
+        except GeocoderTimedOut:
+            attempt += 1
+            print(f"Timeout occurred for {location}. Retrying... (Attempt {attempt})")
+            time.sleep(2 ** attempt)  # Exponential backoff
+        except Exception as e:
+            print(f"Exception: {e}")
+            return ("Unknown", "Unknown")
+    return ("Unknown", "Unknown")
+
 
 def transform_jobs(df_jobs: pd.DataFrame) -> pd.DataFrame:
     """Transform the raw dataframes."""
     pd.options.mode.chained_assignment = None  # default='warn'
-
-    # set to lowercase
-    # df_jobs["location"] = df_jobs["location"].str.lower()
-
-    # how many rows are in df_jobs
-    print("Number of rows in df_jobs:", len(df_jobs))
-    # list all columns in df_jobs
-    print(df_jobs.columns)
 
     df_jobs_renamed = df_jobs.rename(
         columns={"id": "job_id",
@@ -67,19 +102,101 @@ def transform_jobs(df_jobs: pd.DataFrame) -> pd.DataFrame:
                  "date_posted": "date_posted",
                  "keywords": "keywords",
                  "source": "source",
-
                  }
     )
-
     df_jobs_renamed["job_id"] = df_jobs_renamed["job_id"].astype(str)
-    # df_jobs_renamed = df_jobs_renamed.set_index(["job_id"])
+
+    # Set all columns to lowercase 
+    df_jobs_renamed.columns = map(str.lower, df_jobs_renamed.columns)
+
+    # Handle location column
+    df_jobs_renamed[['city', 'country']] = df_jobs_renamed['job_location'].apply(lambda x: pd.Series(_parse_location(x)))
+
+    # Set values in all columns to lowercase
+    df_jobs_renamed = df_jobs_renamed.apply(lambda x: x.astype(str).str.lower())
+
+    df_jobs_renamed.to_csv("jobs.csv", index=False)
 
     return df_jobs_renamed
+class SqlTransform:
+    def __init__(
+        self,
+        postgresql_client: PostgreSqlClient,
+        environment: Environment,
+        table_name: str,
+    ):
+        self.postgresql_client = postgresql_client
+        self.environment = environment
+        self.table_name = table_name
+        self.template = self.environment.get_template(f"{table_name}.sql")
+
+    def create_table_as(self) -> None:
+        """
+        Drops the table if it exists and creates a new copy of the table using the provided select statement.
+        """
+        exec_sql = f"""
+            drop table if exists {self.table_name};
+            create table {self.table_name} as (
+                {self.template.render()}
+            )
+        """
+        self.postgresql_client.execute_sql(exec_sql)
+
+def transform(dag: TopologicalSorter):
+    """
+    Performs `create table as` on all nodes in the provided DAG.
+    """
+    dag_rendered = tuple(dag.static_order())
+    for node in dag_rendered:
+        node.create_table_as()
+
+def load(
+    df: pd.DataFrame,
+    postgresql_client: PostgreSqlClient,
+    table: Table,
+    metadata: MetaData,
+    load_method: str = "upsert",
+) -> None:
+    """
+    Load dataframe to either a database.
+    """
+
+    if load_method == "insert":
+        postgresql_client.insert(
+            data=df.to_dict(orient="records"), table=table, metadata=metadata
+        )
+    elif load_method == "upsert":
+
+        postgresql_client.upsert(
+            data=df.to_dict(orient="records"), table=table, metadata=metadata
+        )
+    elif load_method == "overwrite":
+        postgresql_client.overwrite(
+            data=df.to_dict(orient="records"), table=table, metadata=metadata
+        )
+    else:
+        raise Exception(
+            "Please specify a correct load method: [insert, upsert, overwrite]"
+        )
 
 
+# we might not need the func below:
 def transform_population(df_population: pd.DataFrame) -> pd.DataFrame:
     """Transform the raw dataframes."""
     pd.options.mode.chained_assignment = None  # default='warn'
+
+    # Set all columns to lowercase 
+    df_population.columns = map(str.lower, df_population.columns)
+
+    # Handle location column
+    df_population[['city', 'country']] = df_population['city'].apply(lambda x: pd.Series(_parse_location(x)))
+
+    # Set values in all columns to lowercase
+    df_jobs_renamed = df_population.apply(lambda x: x.astype(str).str.lower())
+    print(df_jobs_renamed.head())
+
+    df_jobs_renamed.to_csv("pop.csv", index=False)
+    
 
     # set to lowercase
     # df_population["location"] = df_population["location"].str.lower()
@@ -134,33 +251,3 @@ def transform_population(df_population: pd.DataFrame) -> pd.DataFrame:
     # print("Number of rows in df_selected:", len(df_selected))
 
     return df_population
-
-
-def load(
-    df: pd.DataFrame,
-    postgresql_client: PostgreSqlClient,
-    table: Table,
-    metadata: MetaData,
-    load_method: str = "upsert",
-) -> None:
-    """
-    Load dataframe to either a database.
-    """
-
-    if load_method == "insert":
-        postgresql_client.insert(
-            data=df.to_dict(orient="records"), table=table, metadata=metadata
-        )
-    elif load_method == "upsert":
-
-        postgresql_client.upsert(
-            data=df.to_dict(orient="records"), table=table, metadata=metadata
-        )
-    elif load_method == "overwrite":
-        postgresql_client.overwrite(
-            data=df.to_dict(orient="records"), table=table, metadata=metadata
-        )
-    else:
-        raise Exception(
-            "Please specify a correct load method: [insert, upsert, overwrite]"
-        )
